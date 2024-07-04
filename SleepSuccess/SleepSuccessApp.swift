e80f47bd6d3dfb50685368a9f22b76047bda1d9d
OGGymBro <143493367+OGGymBro@users.noreply.github.com>
2024-07-04 20:15:22 +0100

    Initial Commit

---
+++ SleepSuccess/SleepSuccessApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepSuccessApp.swift
+//  SleepSuccess
+//
+//  Created by Joaquim Menezes on 04/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SleepSuccessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
